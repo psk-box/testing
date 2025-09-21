d4c7f3b104d2be7bfe66f018d9b5de4780f7095c
PSK <psk@PSKs-MacBook-Pro.local>
2025-09-21 18:22:17 +0530

    Correct changes

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,14 +1,22 @@
-package programs_basics;
-
 public class LinearSearch {
 
 	public static void main(String[] args) {
 		int a[] = {25,6,42,19,26,12};
-		
 		int ele = 26;
-		
-		
+		int index = -1;
 
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == ele) {
+				index = i;
+				break;
+			}
+		}
+
+		if (index != -1) {
+			System.out.println("Element found at index: " + index);
+		} else {
+			System.out.println("Element not found");
+		}
 	}
 
 }
